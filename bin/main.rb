ca5f6bf4f75ba15125d3804e13121c69d5f82879
FrederickMih <fredtimermih@gmail.com>
2021-01-30 13:34:25 +0100

    added method make_first_move

--- bin/main.rb
+++ bin/main.rb
@@ -1,27 +1,18 @@
 #!/usr/bin/env ruby
 
 class TicTacToe
-attr_accessor :board, :turn, :player_one, :player_two
+  attr_accessor :board, :turn, :player_one, :player_two
 
-def initialize(board = nil, turn)
-   @board = board || %w(-)*9
-   @turn = turn
-   @played = []
-end
+  def initialize(board, turn)
+    @board = board || %w[-] * 9
+    @turn = turn
+    @played = []
+  end
 
-
-# Get users names
-def user_info
-   puts "Welcome to your favourite game Tic_Tac_Toe"
-   puts "Kindly enter the player's name in the dialogs below please!"
-   puts "Player one's name"
-   @player_one = gets.chomp
-   puts "Player two's name"
-   @player_two = gets.chomp
-end
-
-def instructions
-    puts "<------> WELCOME TO TIC-TAC-TOE GAME <---------->"
+ 
+   # instructions on how to play this game
+  def instructions
+    puts '<------> WELCOME TO TIC-TAC-TOE GAME <---------->'
     puts '            How this game works?'
     puts '      You play by putting a mark from 0~8'
     puts '               0 | 1 | 2'
@@ -29,12 +20,29 @@ def instructions
     puts '               3 | 4 | 5'
     puts '              --- --- ---'
     puts '               6 | 7 | 8'
-    puts 
+    puts
   end
 
-def make_first_move
-   puts "Please make your move \n#{@player_one}"
-   @user_one_move = gets.chomp.to_i
-end
+   # Get users names and assign their symbol
+  def user_info
+    puts 'Welcome to your favourite game Tic_Tac_Toe'
+    puts "Kindly enter the player's name in the dialogs below please!"
+    puts "Player one's name"
+    @player_one = gets.chomp
+    puts "#{player_one} Please used the symbol [X] to play"
+    puts "Player two's name"
+    @player_two = gets.chomp
+    puts "#{player_two} Please used the symbol [O] to play"
+  end
+      #  Ask players to make moves
 
+  def make_first_move
+    puts "Please make your move #{@player_one} from 1~8"
+    @user_one_move = gets.chomp.to_i
+    if move_valid?(@user_one_move)
+       puts "#{user_one_move} will be place in position #{user_one_move}."
+    else
+      puts "Incorrect move. Try again!"
+    end
+  end
 end
