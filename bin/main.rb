047f7501c54a209271e7c6cb9e783be5a277bc4b
FrederickMih <fredtimermih@gmail.com>
2021-01-30 07:54:14 +0100

    add the initialize method

--- bin/main.rb
+++ bin/main.rb
@@ -3,4 +3,16 @@
 class TicTacToe
 attr_accessor :board, :turn, :player_one, :player_two
 
+def initialize(board = nil, turn)
+   @board = board || %w(-)*9
+   @turn = turn
+   @played = []
+end
+
+
+
+
+
+
+
 end
