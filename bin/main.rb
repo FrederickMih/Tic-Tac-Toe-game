624ba3b54251995ab7fee50e0d2ea2c2eaf955be
FrederickMih <fredtimermih@gmail.com>
2021-02-02 13:44:12 +0100

    add the class Play in the file game_play

--- bin/main.rb
+++ bin/main.rb
@@ -111,13 +111,7 @@ class TicTacToe
       @player_one
     end
   end
-
-  def display_board
-    # puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    # puts '--- --- ---'
-    # puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    # puts '--- --- ---'
-    # puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  display_board
     puts ' 1 | 2 | 3 '
     puts '--------- '
     puts ' 4 | 5 | 6 '
---
+++ lib/play_game.rb
@@ -0,0 +1,4 @@
+class Play 
+
+   
+end
