4e785f9d52fe3b73c0f5c51a8d0c8e7dfa80aee2
FrederickMih <fredtimermih@gmail.com>
2021-02-11 11:34:39 +0100

    added negative test for won method

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,8 +5,11 @@ require_relative '../lib/tictactoe.rb'
 describe Play do
    let(:player) {Play.new}
    describe '#won?' do
-      it 'compare two arrays and return true if there is a winning combination' do
-         expect(player.won?([1, 2, 4, 3])).to eql(true)
+      it 'compare two arrays and display you won if there is a winning combination' do
+         expect(player.won?([1, 2, 8, 3])).to eql(true)
+      end
+       it 'compares two arrays and return false if there is not a winning com' do
+         expect(player.won?([1, 2])).not_to eql(true)
       end
    end
 end
