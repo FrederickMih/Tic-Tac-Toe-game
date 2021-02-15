e58f3eb508e889916090235e5f7efc01e06dfa41
prabhakarzx <ppandey2098@gmail.com>
2021-02-15 13:45:56 +0530

    Testing for returning the player symbol

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -13,3 +13,19 @@ describe Play do
     end
   end
 end
+
+describe Board do
+  let(:board) { Board.new }
+  describe '#switch_turn' do
+    it 'Returns the player symbol' do
+      expect(board.switch_turn(3, 2)).to eql('X')
+    end
+
+    it 'Returns the player symbol' do
+      expect(board.switch_turn(4, 3)).to eql('O')
+    end
+    it 'Does not returns the player symbol' do
+      expect(board.switch_turn(4, 3)).not_to eql('X')
+    end
+  end
+end
