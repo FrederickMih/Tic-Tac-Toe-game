86e733c35c5f4fec586d1f6d08c7cd8f3c5e126a
prabhakarzx <ppandey2098@gmail.com>
2021-02-15 22:36:13 +0530

    Fix linter issues

--- spec/board_rspec.rb
+++ spec/board_rspec.rb
@@ -1,24 +1,24 @@
 require_relative '../lib/board'
 
 describe Board do
-    let(:board) { Board.new }
+  let(:board) { Board.new }
 
-    describe '#display_board' do
-      it 'Display the Board' do
-        expect(true).to  eql(true)
-      end
-    end
-    
-    describe '#switch_turn' do
-      it 'Returns the player symbol' do
-        expect(board.switch_turn(3, 2)).to eql('X')
-      end
-  
-      it 'Returns the player symbol' do
-        expect(board.switch_turn(4, 3)).to eql('O')
-      end
-      it 'Does not returns the player symbol' do
-        expect(board.switch_turn(4, 3)).not_to eql('X')
-      end
+  describe '#display_board' do
+    it 'Display the Board' do
+      expect(true).to eql(true)
     end
   end
+
+  describe '#switch_turn' do
+    it 'Returns the player symbol' do
+      expect(board.switch_turn(3, 2)).to eql('X')
+    end
+
+    it 'Returns the player symbol' do
+      expect(board.switch_turn(4, 3)).to eql('O')
+    end
+    it 'Does not returns the player symbol' do
+      expect(board.switch_turn(4, 3)).not_to eql('X')
+    end
+  end
+end
