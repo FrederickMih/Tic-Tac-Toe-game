ba50d594bf81c1d3c1cc1f5c1ed752e4e3b39179
prabhakarzx <ppandey2098@gmail.com>
2021-02-15 13:43:57 +0530

    Testing for the winnig move

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,3 +2,14 @@ require_relative '../lib/board'
 require_relative '../lib/play_game'
 require_relative '../lib/tictactoe'
 
+describe Play do
+  let(:player) { Play.new }
+  describe '#won?' do
+    it 'Display winning player after comparing' do
+      expect(player.won?([2, 3, 8, 5])).to eql(true)
+    end
+    it 'Return False if not a winning move' do
+      expect(player.won?([2, 3])).not_to eql(true)
+    end
+  end
+end
