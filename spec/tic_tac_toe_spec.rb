8447cb485112c6d27ef50b0e24f2ab5b97827b86
prabhakarzx <ppandey2098@gmail.com>
2021-02-15 13:49:30 +0530

    Testing for the invalid move and move between 1-9

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -29,3 +29,25 @@ describe Board do
     end
   end
 end
+
+describe TicTacToe do
+  let(:tic_tac_toe) { TicTacToe.new }
+  describe '#valid_move' do
+    it 'Invalid move check if the move is not available' do
+      tic_tac_toe.valid_move(2, tic_tac_toe.one_acc, 2)
+      expect(tic_tac_toe.valid_move(2, [], 2)).to eql(false)
+    end
+
+    it 'Valid move check if the move is between 1-9' do
+      expect(tic_tac_toe.valid_move(5, [], 6)).to eql(true)
+    end
+
+    it 'Return Invalid move if the move is not between 1-9' do
+      expect(tic_tac_toe.valid_move(17, [], 6)).to_not eql(true)
+    end
+
+    it 'Invalid move check if the move is not a word' do
+      expect(tic_tac_toe.valid_move('word', [], 6)).to_not eql(true)
+    end
+  end
+end
