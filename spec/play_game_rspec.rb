7a7d89b166d15521cd765c940c52c7826b2db93d
prabhakarzx <ppandey2098@gmail.com>
2021-02-15 15:10:33 +0530

    Rspec addition for the play game

--- spec/play_game_rspec.rb
+++ spec/play_game_rspec.rb
@@ -4,6 +4,34 @@ require_relative '../lib/tictactoe'
 
 describe Play do
   let(:player) { Play.new }
+  let(:board) { Play.new }
+
+  describe "#initialize" do
+    it "Initialize the board and check the winnig combilation" do
+      expect([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+      ]).to  eql([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+      ])
+    end
+    
+  end
+  
+
   describe '#won?' do
     it 'Display winning player after comparing' do
       expect(player.won?([2, 3, 8, 5])).to eql(true)
